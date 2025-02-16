bafd78994b1c33a285e29a9bd5cf65409ae2d806
WJijon223 <williamjijon223@gmail.com>
2025-02-15 21:44:54 -0500

    added the layout for the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Header } from "./components/Header";
+import { SideNav } from "./components/SideNav";
+import { PokeCard } from "./components/PokeCard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <SideNav />
+      <PokeCard />
+    </>
+  );
 }
 
 export default App;
